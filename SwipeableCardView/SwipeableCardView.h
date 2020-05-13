994d909743d03c786fa5f829b9f162af438435cd
Katalin Neda <katalin.neda@halcyonmobile.com>
2020-05-13 15:13:53 +0300

    Add files

---
+++ SwipeableCardView/SwipeableCardView.h
@@ -0,0 +1,19 @@
+//
+//  SwipeableCardView.h
+//  SwipeableCardView
+//
+//  Created by Katalin Neda on 13/05/2020.
+//  Copyright © 2020 Katalin Neda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwipeableCardView.
+FOUNDATION_EXPORT double SwipeableCardViewVersionNumber;
+
+//! Project version string for SwipeableCardView.
+FOUNDATION_EXPORT const unsigned char SwipeableCardViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeableCardView/PublicHeader.h>
+
+
---
+++ SwipeableCardViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeableCardViewExample
+//
+//  Created by Katalin Neda on 13/05/2020.
+//  Copyright © 2020 Katalin Neda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
