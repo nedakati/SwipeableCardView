2956aa9fe99fe6d87c79cdf97ce896fa316e095a
Katalin Neda <katalin.neda@halcyonmobile.com>
2020-05-13 15:23:30 +0300

    Add Example project

---
+++ SwipeableCardView/SwipeableCard.swift
@@ -0,0 +1,206 @@
+//
+//  SwipeableCardView.swift
+//  SwipeableCardView
+//
+//  Created by Katalin Neda on 13/05/2020.
+//  Copyright © 2020 Katalin Neda. All rights reserved.
+//
+
+import UIKit
+
+public protocol SwipeableCardDelegate: AnyObject {
+    func didRemoveCard(_ card: SwipeableCard)
+    func didSwipeRight(_ card: SwipeableCard)
+    func didSwipeLeft(_ card: SwipeableCard)
+    func didTapOnCard(_ card: SwipeableCard)
+}
+
+public class SwipeableCard: UIView {
+
+    // MARK: - Private properties
+    
+    private var shadowView: UIView!
+    private var contentView: UIView!
+    private var overlay: UIView!
+    
+    private var topConstraint: NSLayoutConstraint?
+    private var heightConstraint: NSLayoutConstraint?
+    private var leadingConstraint: NSLayoutConstraint?
+    private var trailingConstraint: NSLayoutConstraint?
+
+    weak var delegate: SwipeableCardDelegate?
+
+    // MARK: - Init
+    
+    init(content: UIView) {
+        self.contentView = content
+        super.init(frame: .zero)
+        
+        addShadow()
+        configureContentView()
+        addOverlay()
+        addGesture()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Public methods
+    
+    public func addTopAnchor(related view: UIView, constant: CGFloat) {
+        if let topConstraint = topConstraint {
+            topConstraint.constant = constant
+            return
+        }
+        self.topConstraint = topAnchor.constraint(equalTo: view.topAnchor, constant: constant)
+        NSLayoutConstraint.activate([topConstraint!])
+    }
+    
+    public func addLeadingAnchor(related view: UIView, constant: CGFloat) {
+        if let leadingConstraint = leadingConstraint {
+            leadingConstraint.constant = constant
+            return
+        }
+        self.leadingConstraint = leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: constant)
+        NSLayoutConstraint.activate([leadingConstraint!])
+    }
+    
+    public func addTrailingAnchor(related view: UIView, constant: CGFloat) {
+        if let trailingConstraint = trailingConstraint {
+            trailingConstraint.constant = -constant
+            return
+        }
+        self.trailingConstraint = trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -constant)
+        NSLayoutConstraint.activate([trailingConstraint!])
+    }
+    
+    public func addHeightAnchor(constant: CGFloat) {
+        if let heightConstraint = heightConstraint {
+            heightConstraint.constant = constant
+            return
+        }
+        self.heightConstraint = heightAnchor.constraint(equalToConstant: constant)
+        NSLayoutConstraint.activate([heightConstraint!])
+    }
+    
+    public func removeOverlay() {
+        overlay.isHidden = true
+    }
+    
+    // MARK: - Private methods
+    
+    private func addShadow() {
+        shadowView = UIView()
+        shadowView.translatesAutoresizingMaskIntoConstraints = false
+        shadowView.backgroundColor = .clear
+        shadowView.layer.shadowColor = UIColor.gray.cgColor
+        shadowView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        shadowView.layer.shadowOpacity = 0.4
+        shadowView.layer.shadowRadius = 10
+        addSubview(shadowView)
+           
+        NSLayoutConstraint.activate([
+            shadowView.leftAnchor.constraint(equalTo: leftAnchor),
+            shadowView.rightAnchor.constraint(equalTo: rightAnchor),
+            shadowView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            shadowView.topAnchor.constraint(equalTo: topAnchor)
+        ])
+    }
+    
+    private func addOverlay() {
+        overlay = UIView()
+        overlay.translatesAutoresizingMaskIntoConstraints = false
+        overlay.backgroundColor = UIColor.white.withAlphaComponent(0.3)
+        overlay.layer.cornerRadius = 10
+        overlay.clipsToBounds = true
+        addSubview(overlay)
+           
+        NSLayoutConstraint.activate([
+            overlay.leftAnchor.constraint(equalTo: leftAnchor),
+            overlay.rightAnchor.constraint(equalTo: rightAnchor),
+            overlay.bottomAnchor.constraint(equalTo: bottomAnchor),
+            overlay.topAnchor.constraint(equalTo: topAnchor)
+        ])
+    }
+    
+    private func configureContentView() {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.layer.cornerRadius = 10
+        contentView.clipsToBounds = true
+        shadowView.addSubview(contentView)
+           
+        NSLayoutConstraint.activate([
+            contentView.leftAnchor.constraint(equalTo: shadowView.leftAnchor),
+            contentView.rightAnchor.constraint(equalTo: shadowView.rightAnchor),
+            contentView.bottomAnchor.constraint(equalTo: shadowView.bottomAnchor),
+            contentView.topAnchor.constraint(equalTo: shadowView.topAnchor)
+        ])
+    }
+    
+    private func addGesture() {
+        isUserInteractionEnabled = true
+        addGestureRecognizer(UILongPressGestureRecognizer(target: self, action: #selector(handleLongTapGesture)))
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapGesture)))
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeLeft.direction = .left
+        addGestureRecognizer(swipeLeft)
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeRight.direction = .right
+        addGestureRecognizer(swipeRight)
+    }
+    
+    // MARK: - UI Actions
+    
+    @objc private func handleTapGesture(sender: UITapGestureRecognizer) {
+        delegate?.didTapOnCard(self)
+    }
+    
+    @objc private func handleLongTapGesture(sender: UILongPressGestureRecognizer) {
+
+        guard let card = sender.view as? SwipeableCard else { return }
+
+        switch sender.state {
+        case .began:
+            UIView.animate(withDuration: 0.1) {
+                self.overlay.isHidden = false
+                card.transform = CGAffineTransform(scaleX: 1.005, y: 1.005)
+            }
+        case .ended:
+            UIView.animate(withDuration: 0.1) {
+                self.overlay.isHidden = true
+                card.transform = .identity
+            }
+        default:
+            break
+        }
+    }
+    
+    @objc private func handleSwipe(sender: UISwipeGestureRecognizer) {
+
+        guard let card = sender.view as? SwipeableCard else { return }
+        let center = CGPoint(x: frame.width / 2, y: frame.height / 2)
+
+        if sender.direction == .right {
+            UIView.animate(withDuration: 0.3, animations: {
+                card.center = CGPoint(x: center.x + 400, y: center.y + 30)
+                card.alpha = 0
+                self.overlay.isHidden = true
+            }) { _ in
+                self.delegate?.didSwipeRight(self)
+                self.delegate?.didRemoveCard(self)
+            }
+        } else {
+            UIView.animate(withDuration: 0.3, animations: {
+                card.center = CGPoint(x: center.x - 400, y: center.y + 30)
+                card.alpha = 0
+                self.overlay.isHidden = true
+            }) { _ in
+                self.delegate?.didSwipeLeft(self)
+                self.delegate?.didRemoveCard(self)
+            }
+        }
+    }
+}
---
+++ SwipeableCardViewExample/ViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ViewModel.swift
+//  SwipeableCardViewExample
+//
+//  Created by Katalin Neda on 13/05/2020.
+//  Copyright © 2020 Katalin Neda. All rights reserved.
+//
+
+import UIKit
+
+struct City {
+    let image: UIImage?
+    let title: String
+}
+
+class ViewModel {
+    
+    var cities: [City] = []
+    
+    init() {
+        cities.append(City(image: UIImage(named: "amsterdam"), title: "Amsterdam"))
+        cities.append(City(image: UIImage(named: "barcelona"), title: "Barcelona"))
+        cities.append(City(image: UIImage(named: "mountain"), title: "Austria"))
+        cities.append(City(image: UIImage(named: "paris"), title: "Paris"))
+        cities.append(City(image: UIImage(named: "svajc"), title: "Switzerland"))
+        cities.append(City(image: UIImage(named: "london"), title: "London"))
+    }
+    
+}
